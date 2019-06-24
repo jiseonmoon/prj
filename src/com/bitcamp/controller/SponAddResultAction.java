a2fa7703dca63641f39cd438e7026cbe1cf94ea4
hong <hong@naver.com>
2019-06-24 17:30:25 +0900

    시험 전 세이브

--- src/com/bitcamp/controller/SponAddResultAction.java
+++ src/com/bitcamp/controller/SponAddResultAction.java
@@ -5,9 +5,13 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.bitcamp.comm.Action;
 import com.bitcamp.comm.ForwardAction;
+import com.bitcamp.dto.BoardDTO;
+import com.bitcamp.dto.MemberDTO;
+import com.bitcamp.service.BoardService;
 
 public class SponAddResultAction implements Action {
 
@@ -15,7 +19,23 @@ public class SponAddResultAction implements Action {
 	public ForwardAction execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		return null;
+		request.setCharacterEncoding("utf-8");
+		HttpSession session = request.getSession(false);
+		MemberDTO member = (MemberDTO)session.getAttribute("member");
+		
+		
+		BoardDTO dto = new BoardDTO();
+		dto.setBoardtitle(title);
+		dto.setBoardwriter(writer);
+		dto.setBoardcontent(content);
+		
+		BoardService service=BoardService.getInstance();
+		service.AddService(dto);
+		ForwardAction forward=new ForwardAction();
+		forward.setRedirect(true);
+		forward.setPath("list.do");
+		
+		return forward;
 	}
 
 }
--- src/com/bitcamp/dto/MemberDTO.java
+++ src/com/bitcamp/dto/MemberDTO.java
@@ -1,5 +1,14 @@
 package com.bitcamp.dto;
 
 public class MemberDTO {
+	private int mid;
 
+	public int getMid() {
+		return mid;
+	}
+
+	public void setMid(int mid) {
+		this.mid = mid;
+	}
+	
 }
