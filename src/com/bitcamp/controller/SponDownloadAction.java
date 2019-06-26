0b23d2158c06fa63fd0e5978d338e93eaf24de58
hong <hong@naver.com>
2019-06-26 16:14:04 +0900

    Download 처리합니다.

--- src/com/bitcamp/controller/SponDownloadAction.java
+++ src/com/bitcamp/controller/SponDownloadAction.java
@@ -9,6 +9,7 @@ import java.net.URLEncoder;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -96,38 +97,63 @@ public class SponDownloadAction implements Action {
 		System.out.println("list size : " + list.size());	
 		
 		//excel 정보 작성
-		String realPath = request.getServletContext().getRealPath("/upload");
+		String folder = request.getServletContext().getRealPath("/upload");
 		JexcelWriter jx = JexcelWriter.getInstance();
-		String filename = jx.createExl(list, dto.getMno(), realPath);
-		
-		//다운로드 처리
-		File file = new File(realPath + "\\" + filename);
-		long length = file.length();
-		response.setContentType("application/octet-stream");
-		response.setContentLengthLong(length);
-		boolean isIe = request.getHeader("user-agent").toUpperCase().indexOf("MSIE") != -1 || request.getHeader("user-agent").indexOf("11.0") != -1;
-		if(isIe) {
-			filename = URLEncoder.encode(filename, "UTF-8");
-		}else {
-			filename = new String(filename.getBytes("UTF-8"),"8859_1");
-		}
-		response.setHeader("Content-Disposition", "attachment;filename" + filename);
-		BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
-		PrintWriter out = 
-		//파일 삭제
-		int deleteok = jx.deleteExl(realPath + "\\" + filename);
-		
+		String fileName = jx.createExl(list, dto.getMno(), folder);
+			
 		request.setAttribute("list", list);
 		request.setAttribute("pageinfo", pageinfo);
 		request.setAttribute("tag", tag);
 		request.setAttribute("isFinish", isFinish);
 		request.setAttribute("search", search);
 		
-		ForwardAction forward = new ForwardAction();
-	    forward.setRedirect(false);
-	    forward.setPath("/WEB-INF/board/templete.jsp?page=sponlist.jsp");
-	      
-	    return forward;
+		// 파일이 있는 절대경로를 가져온다.
+		// 현재 업로드된 파일은 UploadFolder 폴더에 있다.
+		
+		// 파일의 절대경로를 만든다.
+		String filePath = folder + "/" + fileName;
+
+		try {
+			File file = new File(filePath);
+			byte b[] = new byte[(int) file.length()];
+			
+			// page의 ContentType등을 동적으로 바꾸기 위해 초기화시킴
+			response.reset();
+			response.setContentType("application/octet-stream");
+			
+			// 한글 인코딩
+			String encoding = new String(fileName.getBytes("utf-8"),"8859_1");
+			
+			// 파일 링크를 클릭했을 때 다운로드 저장 화면이 출력되게 처리하는 부분
+			response.setHeader("Content-Disposition", "attachment;filename="+ encoding);
+			response.setHeader("Content-Length", String.valueOf(file.length()));
+			
+			if (file.isFile()) // 파일이 있을경우
+			{
+				FileInputStream fileInputStream = new FileInputStream(file);
+				ServletOutputStream servletOutputStream = response.getOutputStream();
+				
+				//  파일을 읽어서 클라이언트쪽으로 저장한다.
+				int readNum = 0;
+				while ( (readNum = fileInputStream.read(b)) != -1) {
+					servletOutputStream.write(b, 0, readNum);
+				}
+				
+				servletOutputStream.close();
+				fileInputStream.close();
+			}else {
+				ForwardAction forward = new ForwardAction();
+			    forward.setRedirect(false);
+			    forward.setPath("/WEB-INF/board/templete.jsp?page=sponlist.jsp");
+			      
+			    return forward;
+			}
+			
+		} catch (Exception e) {
+			System.out.println("Download Exception : " + e.getMessage());
+		}
+		
+		return null;
 	}
 
 }
